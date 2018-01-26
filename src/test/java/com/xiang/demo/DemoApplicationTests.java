d78b045e797f40d57128c097c332daf076efd442
“me <jiangcheng.xiang@qq.com>
2018-01-26 17:37:00 +0800

    Spring Boot中使用JavaMailSender发送邮件

--- src/test/java/com/xiang/demo/DemoApplicationTests.java
+++ src/test/java/com/xiang/demo/DemoApplicationTests.java
@@ -9,12 +9,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -29,6 +36,9 @@ public class DemoApplicationTests {
 
 	@Autowired
 	private StringRedisTemplate stringRedisTemplate;
+
+	@Autowired
+	private JavaMailSender mailSender;
 
 	@Test
 	public void contextLoads() {
@@ -60,5 +70,35 @@ public class DemoApplicationTests {
 		stringRedisTemplate.opsForValue().set("name", "大帅比");
 		Assert.assertEquals("大帅比", stringRedisTemplate.opsForValue().get("name"));
 	}
+
+	@Test
+	public void sendSimpleMail() throws Exception {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("1150241658@qq.com");
+		message.setTo("228873501@qq.com");
+		message.setSubject("主题：简单邮件");
+		message.setText("测试邮件内容!");
+
+		mailSender.send(message);
+	}
+
+	@Test
+	public void sendInlineMail() throws Exception {
+
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+		helper.setFrom("jiangcheng.xiang@qq.com");
+		helper.setTo("228873501@qq.com");
+		helper.setSubject("主题：嵌入静态资源");
+		helper.setText("<html><body><img src=\"cid:weixin\" ></body></html>", true);
+
+		FileSystemResource file = new FileSystemResource(new File("weixin.jpg"));
+		helper.addInline("weixin", file);
+
+		mailSender.send(mimeMessage);
+
+	}
+
 
 }
